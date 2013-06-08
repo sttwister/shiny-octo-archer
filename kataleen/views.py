615dc48a0e380e97e2b287b075420ad6920c1fe7
Felix Kerekes <sttwister@gmail.com>
2013-06-08 10:48:31 +0300

    Time test

--- kataleen/views.py
+++ kataleen/views.py
@@ -3,6 +3,15 @@ from pytz import timezone
 
 from django.http import HttpResponse
 
+bucharest = timezone('Europe/Bucharest')
+deadline = datetime(2013, 6, 8, 11, 0)
+deadline = bucharest.localize(deadline)
+
 def home(request):
-    bucharest = timezone('Europe/Bucharest')
-    return HttpResponse('%s' % datetime.now(bucharest))
+    amu = datetime.now(bucharest)
+    remaining = amu - deadline
+    if amu<deadline:
+        return HttpResponse('too soon')
+    else:
+        return HttpResponse('is ok')
+
